c87ed7405822dd1b41627b9f62089e2b264bf662
ishmam999 <ishmam2050@gmail.com>
2019-10-03 14:24:20 +0900

    database initial

--- database/migrations/2019_10_03_040851_group.php
+++ database/migrations/2019_10_03_040851_group.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class Group extends Migration
 {
     /**
      * Run the migrations.
@@ -13,15 +13,12 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('groups', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
-            $table->string('stuff_id')->unique();
+            $table->string('group_num');
             $table->string('role');
-            $table->string('image')->default('profile.png');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('location');
+            $table->date('class_time');
             $table->rememberToken();
             $table->timestamps();
         });
@@ -34,6 +31,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('groups');
     }
 }
